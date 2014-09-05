598db88468c3f8a8c1512b736b344c649e676bfb
Lars Janssen <lars@fazy.net>
2014-09-05 17:02:08 +0100

    Adding blank bundle, composer.json and .gitignore

---
+++ FazyAsseticConfigBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Fazy\AsseticConfigBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * FazyAsseticConfigBundle
+ */
+class FazyAsseticConfigBundle extends Bundle
+{
+}
